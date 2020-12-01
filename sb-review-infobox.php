57421e2abf5fec945f908be2bb21387ef0d78558
CE <archmagece@users.noreply.github.com>
2020-12-01 22:43:49 +0900

    movie -> review

--- sb-review-infobox.php
+++ sb-review-infobox.php
@@ -1,13 +1,13 @@
 <?php
 /*
 * Plugin Name: SB-Review-Infobox
-* Plugin URI: http://wp.scriptonbasestar.com/plugin
-* Description: ScriptonBasestar Inc. wordpress review plugin. usage : [sb_review_infobox_from_imdb id="imdb id"]
-* Version: 0.1
+* Plugin URI: http://scriptonbasestar.com/wp/plugin
+* Description: ScriptonBasestar Inc. wordpress review info plugin. usage : [sb_review_infobox id="imdb id" type="imdb"]
+* Version: 0.2
 * Text Domain: sb-review-infobox
 * Domain Path: /languages/
 * Author: archmagece
-* Author URI: http://scriptonbasestar.com
+* Author URI: http://scriptonbasestar.org
 * License: GPLv3
 * License URI: http://www.gnu.org/licenses/gpl-3.0
 * Slug: sb-review-infobox
@@ -20,24 +20,14 @@ $upload_dir = wp_upload_dir();
 define('SB_CACHE_DIR', $upload_dir['basedir'] . "/sb_cache");
 define('SB_CACHE_URL', $upload_dir['baseurl'] . "/sb_cache");
 
-include_once("inc/phpQuery-onefile.php");
 //include("classes/filter-imdb-button.php");
-include("classes/shortcodes-movie-imdb.php");
-include("classes/shortcodes-movie-naver.php");
-include("classes/shortcodes-movie-kobis.php");
-include("classes/shortcodes-book-interpark.php");
-wp_enqueue_style( 'shortcodes-movie', plugins_url( 'css/shortcodes-movie.css', __FILE__ ));
-wp_enqueue_style( 'shortcodes-book', plugins_url( 'css/shortcodes-book.css', __FILE__ ));
-//wp_enqueue_style( 'stylesheet', plugins_url( 'shortcodes-movie.css', __FILE__ ));
+include("classes/shortcodes-imdb.php");
+
 
 
 function sb_review_infobox_activate()
 {
-    add_option('sbcacheage', -1);
-    add_option('sbimdbkey', "{your code}")
-    add_option('sbnaverkey', "{your code}");
-    add_option('sbkobiskey', "{your code}");
-    add_option('sbinterparkkey', "{your code}");
+//    add_option('imdbcacheage', -1);
 //    add_option('imdbheadbg', 'FFCC00');
 //    add_option('imdbheadfg', 'FFFFFF');
 //    add_option('imdbbodybg', 'F4F3D9');
@@ -47,32 +37,17 @@ function sb_review_infobox_activate()
         if (!wp_mkdir_p(SB_CACHE_DIR)) {
             die("Unable to create cache directory in uploads folder. Please make sure uploads directory is writable");
         }
-        wp_mkdir_p(SB_CACHE_DIR."/imdb");
-        wp_mkdir_p(SB_CACHE_DIR."/naver");
-        wp_mkdir_p(SB_CACHE_DIR."/kobis");
-        wp_mkdir_p(SB_CACHE_DIR."/interpark");
     }
 }
 
 function sb_review_infobox_deactivate()
 {
-    delete_option('sbcacheage');
-    delete_option('sbimdbkey');
-    delete_option('sbnaverkey');
-    delete_option('sbkobiskey');
-    delete_option('sbinterparkkey');
+//    delete_option('imdbcacheage');
+//    delete_option('imdbheadbg');
 //    delete_option('imdbheadfg');
 //    delete_option('imdbbodybg');
 //    delete_option('imdbbodyfg');
 //    delete_option('imdbcorner');
-//    if (is_dir(SB_CACHE_DIR)) {
-//        if (wp_remomkdir_p(SB_CACHE_DIR)) {
-//            die("Unable to create cache directory in uploads folder. Please make sure uploads directory is writable");
-//        }
-//        wp_remomkdir_p(SB_CACHE_DIR."/imdb");
-//        wp_remomkdir_p(SB_CACHE_DIR."/naver");
-//        wp_remomkdir_p(SB_CACHE_DIR."/kobis");
-//    }
 }
 
 register_activation_hook(__FILE__, 'sb_review_infobox_activate');
